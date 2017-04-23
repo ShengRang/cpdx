31782acc31eaaedd6f07e80a37a975467c94d1b5
ShengRang <541628498@qq.com>
2017-04-23 23:02:26 +0800

    fix stupid

--- cpdx/algorithms/interface.py
+++ cpdx/algorithms/interface.py
@@ -9,7 +9,7 @@ from cpdx.langs.common import parser_dispatch
 class Detector(object):
 
     def __init__(self, lang, prep):
-        self.parser = parser_dispatch(lang, prep)
+        self.parser_cls = lambda: parser_dispatch(lang, prep)
 
     def run(self, files):
         raise NotImplementedError()
